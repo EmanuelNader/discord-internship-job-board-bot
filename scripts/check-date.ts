6b57a42f55237bb8e3a03902fa3735039dcfa793
Emanuel Nader <emanuelnader@users.noreply.github.com>
2026-07-26 21:29:38 -0700

    chore: remove temp script

--- scripts/check-date.ts
+++
@@ -1,4 +0,0 @@
-import { fetchJson } from "@/adapters/base";
-const data = await fetchJson<any>("https://boards-api.greenhouse.io/v1/boards/stripe/jobs?content=Job");
-const job = data.jobs[0];
-console.log(JSON.stringify(job, null, 2));
